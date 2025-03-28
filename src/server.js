73ea1e0a1f4193a0ecf42b7d770597aff560b495
Timofey-Byunenko1 <byunenkotimofey@gmail.com>
2025-03-28 15:47:42 +0200

    add index file

--- src/server.js
+++ src/server.js
@@ -13,6 +13,7 @@ app.use((req, res) => {
     res.status(404).json({ message: "Not found" });
 });
 
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     pino.info(`Server is running on port ${PORT}`);
